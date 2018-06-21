ee67773435854bcb477dcf543c7f62dba1cedfa5
ratibor81 <33425087+ratibor81@users.noreply.github.com>
2018-06-21 14:59:36 +0300

    Add files via upload

--- Project_build_01/js/scripts.js
+++ Project_build_01/js/scripts.js
@@ -5,33 +5,29 @@ var btn_next = document.querySelector('.arr-next');
 var reviews = document.querySelectorAll('.review-block__card');
 var counters = document.querySelectorAll('.review-block__page-counter span');
 
-var i = 0;
+var review = 0;
 var counter = 0;
 
 var prevClickHandler = function prevClickHandler() {
-  reviews[i].style.display = 'none';
-  i -= 1;
-  if (i < 0) {
-    i = reviews.length - 1;
-  }
-  reviews[i].style.display = 'flex';
-  goToCount(counter - 1);
+	goToReview(review - 1);
+	goToCount(counter - 1);
 };
 
 var nextClickHandler = function nextClickHandler() {
-  reviews[i].style.display = 'none';
-  i += 1;
-  if (i >= reviews.length) {
-    i = 0;
-  }
-  reviews[i].style.display = 'flex';
-  goToCount(counter + 1);
+	goToReview(review + 1);
+	goToCount(counter + 1);
 };
 
 var goToCount = function goToCount(n) {
-  counters[counter].classList.remove('through');
-  counter = (n + counters.length) % counters.length;
-  counters[counter].classList.add('through');
+	counters[counter].classList.remove('through');
+	counter = (n + counters.length) % counters.length;
+	counters[counter].classList.add('through');
+};
+
+var goToReview = function goToReview(n) {
+	reviews[review].style.display = 'none';
+	review = (n + reviews.length) % reviews.length;
+	reviews[review].style.display = 'flex';
 };
 
 btn_prev.addEventListener('click', prevClickHandler);
